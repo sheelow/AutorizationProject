cc4b630cdbd41f2e0a2ee856f4a0b6d56dcd269c
Artem Shilov <104136658+sheelow@users.noreply.github.com>
2022-06-15 13:20:13 +0300

    Did some refactoring

--- AutorizationProject/ViewController2.swift
+++ AutorizationProject/ViewController2.swift
@@ -13,14 +13,15 @@ class ViewController2: UIViewController {
     
     @IBOutlet weak var mainImageView: UIImageView!
     
-    let url = URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4zSMz_lrisSh_06BmtSmzzuqYHUcFhN9y3A&usqp=CAU")
+    let url = URL(string: "https://funart.pro/uploads/posts/2020-04/1585767812_28-p-foni-dlya-ipad-76.jpg")
     
     var user: Users = Users(login: "", password: "")
     
     private lazy var label: UILabel = {
         let label = UILabel(frame: CGRect(x: 0 , y: view.frame.midY, width: UIScreen.main.bounds.width, height: 100))
         label.numberOfLines = 0
-        label.textColor = .black
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 28)
         label.textAlignment = .center
         print(user)
         return label
